8c4fb566eb6d271a7821f4d38c5a7eedc7ca6f20
Rahul Shaw <shawrahul2006@gmail.com>
2021-05-22 15:29:57 +0530

    Fix form submission

--- src/script.js
+++ src/script.js
@@ -1,50 +1,54 @@
 function timeFunction() {
-    setTimeout(function(){ alert("After 5 seconds!"); }, 5000);
+  setTimeout(function () {
+    alert("After 5 seconds!");
+  }, 5000);
 }
-document.getElementById("submit").addEventListener('click',()=>{
-    document.getElementById("submit").classList.remove("submit")
-    document.getElementById("submit").classList.add("sub-focus")
-})
-document.getElementById("submit").addEventListener('mouseout',()=>{
-    document.getElementById("submit").classList.remove("sub-focus")
-    document.getElementById("submit").classList.add("submit")
-    
-})
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.getElementById("submit").classList.remove("submit");
+  document.getElementById("submit").classList.add("sub-focus");
+});
+document.getElementById("submit").addEventListener("mouseout", () => {
+  document.getElementById("submit").classList.remove("sub-focus");
+  document.getElementById("submit").classList.add("submit");
+});
 
 var d = new Date();
 var hrs = d.getHours();
 console.log(hrs);
-if(hrs<=19){
-    document.querySelector('body').style['background-image']="url('./assets/sunny.jpeg')";
+if (hrs <= 19) {
+  document.querySelector("body").style["background-image"] =
+    "url('./assets/sunny.jpeg')";
+} else {
+  document.querySelector("body").style["background-image"] =
+    "url('./assets/night.jpg')";
 }
-else{
-    document.querySelector('body').style['background-image']="url('./assets/night.jpg')";
-}
-const city = document.querySelector('#city');
-var button = document.querySelector('#submit');
-const showData = document.querySelector('.showData');
+const city = document.querySelector("#city");
+var button = document.querySelector("#submit");
+const showData = document.querySelector(".showData");
 
 //Get API Key By Login To OpenWeather.org
 const API_Key = "a0e78d3b449db7059df0a38abd3952f8";
 
 //Now Add Event Listener
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
+  //Get Input Value
+  const cityInput = city.value;
+  // console.log(cityInput);
 
-    //Get Input Value
-    const cityInput = city.value;
-    // console.log(cityInput);
+  //Now Fetch Through Get API
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-    //Now Fetch Through Get API 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
+      //When Fill Input Field Then Clear Input Field
+      city.value = " ";
 
-            //When Fill Input Field Then Clear Input Field
-            city.value = " ";
-
-            //Now Show All Data Value
-            showData.innerHTML = `
+      //Now Show All Data Value
+      showData.innerHTML = `
             <ul>
             <br>
             <li class="desc">${data.name}</li>
@@ -55,9 +59,6 @@ button.addEventListener('click', () => {
             <li class="temp">Humidity ${data.main.humidity}</li>
             <li class="temp">Conclusion: ${data.weather[0].description}</li>
         </ul>
-                                `; 
-
-        });
-
+         `;
+    });
 });
-
